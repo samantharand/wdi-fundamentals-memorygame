216b7a5a4800781fbc077960be378b4330f34fd1
Samantha Rand <samantharand@Samanthas-MBP-2.lan>
2020-01-24 12:59:49 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,13 +26,14 @@ const cardsInPlay = [];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!")
+			alert("You found a match!")
 		} else {
-			console.log("Sorry, try again")
+			alert("Sorry, try again")
 		}
 	};
 
-function flipCard(cardId) {
+function flipCard() {
+	const cardId = this.getAttribute("data-id");
 	const cardOne = cards[cardId];
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
@@ -40,10 +41,49 @@ function flipCard(cardId) {
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
+	this.setAttribute("src", cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
 };
 
-	flipCard(0);
-	flipCard(2);
+function createBoard(){
+	for (let i=0; i<cards.length; i++) {
+		let cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
